3873771bcb24d4876d8291d35b6bd1a385082389
Jerry Ranjie Zhao <jerryzhaoranjie@163.com>
2026-05-18 22:19:49 +0800

    Initial commit

---
+++ blindRun/blindRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  blindRunApp.swift
+//  blindRun
+//
+//  Created by Jerry on 5/18/26.
+//
+
+import SwiftUI
+
+@main
+struct blindRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
